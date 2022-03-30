a822d20921b429f7ef9fa277f1e33facaef91230
Mehmet Hanifi Aydın <hnfaydn@gmail.com>
2022-03-30 04:10:18 +0300

    All Requirements done

--- src/main/java/com/turkcell/rentACar/business/requests/cityRequests/CreateCityRequest.java
+++ src/main/java/com/turkcell/rentACar/business/requests/cityRequests/CreateCityRequest.java
@@ -19,6 +19,6 @@ public class CreateCityRequest {
     private int cityId;
 
     @NotNull
-    @Pattern(regexp = "^[a-zA-Z]{2,50}", message = BusinessMessages.CityRequestsMessages.COLOR_NAME_REGEX_MESSAGE)
+    @Pattern(regexp = "^[a-zA-Zi]{2,50}", message = BusinessMessages.CityRequestsMessages.COLOR_NAME_REGEX_MESSAGE)
     private String cityName;
 }
